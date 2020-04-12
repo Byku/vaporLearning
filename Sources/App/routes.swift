f602e3c8fe32cd9da23200dd879bde98d61131af
Dmitry Ivanov <d.ivanov@big3.ru>
2020-04-12 13:49:35 +0300

    move acronyms routers to controller

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,19 +6,7 @@ public func routes(_ router: Router) throws {
     router.get { req in
         "It works!"
     }
-    
-    router.post("api", "acronyms") { req -> Future<Acronym> in
-        return try req.content.decode(Acronym.self)
-            .flatMap(to: Acronym.self, { acronym in
-                return acronym.save(on: req)
-            })
-    }
-    
-    router.get("api", "acronyms") { req -> Future<[Acronym]> in
-        Acronym.query(on: req).all()
-    }
 
-    router.get("api", "acronym", Acronym.parameter) { req -> Future<Acronym> in 
-        try req.parameters.next(Acronym.self)
-    }
+    let acronymsController = AcronymsController()
+    try router.register(collection: acronymsController)
 }
